01e19bebdb43d7a62e92733e288d38e2cd75bdb1
Prashant Singh <pjeet.0733@gmail.com>
2021-04-11 22:07:12 +0400

    Fixed sh file for jenkins

--- src/test/resources/Jenkins/SetupExecution.sh
+++ src/test/resources/Jenkins/SetupExecution.sh
@@ -2,36 +2,36 @@
 PROPERTIESFILEPATH="src/test/resources/Jenkins/FrameworkJenkins.properties"
 
 if [[ $Url ]]; then
-    sed -i'' -e "s,url='$Url',g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,url=,url=$Url,g" $PROPERTIESFILEPATH
 else
     echo "Missing URL for the environment."
     exit 1
 fi
 
 if [[ $Env ]]; then
-    sed -i'' -e "s,env='$Env',g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,env=,env=$Env,g" $PROPERTIESFILEPATH
 else
     echo "Missing environmemnt name."
     exit 1
 fi
 
 if [[ $TesterName ]]; then
-    sed -i'' -e "s,testername='$TesterName',g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,testername=,testername=$TesterName,g" $PROPERTIESFILEPATH
 else
     echo "Missing testername."
     exit 1
 fi
 
 if [[ $DownloadWebDriver ]]; then
-    sed -i'' -e "s,downloadwebdriver='$DownloadWebDriver',g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,downloadwebdriver=,downloadwebdriver=$DownloadWebDriver,g" $PROPERTIESFILEPATH
 else
-    sed -i'' -e "s,downloadwebdriver=no,g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,downloadwebdriver=,downloadwebdriver=no,g" $PROPERTIESFILEPATH
 fi
 
 if [[ $RunMode ]]; then
-    sed -i'' -e "s,runmode='$RunMode',g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,runmode=,runmode$RunMode,g" $PROPERTIESFILEPATH
     if [[ $RemoteUrl ]]; then
-        sed -i'' -e "s,remoteurl='$RemoteUrl',g" $PROPERTIESFILEPATH
+        sed -i'' -e "s,remoteurl=,remoteurl=$RemoteUrl,g" $PROPERTIESFILEPATH
     else
         echo "Missing Remote URL."
         exit 1
@@ -42,72 +42,72 @@ else
 fi
 
 if [[ $UseElk ]]; then
-    sed -i'' -e "s,useelk='$UseElk',g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,useelk=,useelk=$UseElk,g" $PROPERTIESFILEPATH
     if [[ $ElasticSearchUrl ]]; then
-        sed -i'' -e "s,elksuiteurl='$ElasticSearchUrl',g" $PROPERTIESFILEPATH
+        sed -i'' -e "s,elksuiteurl=,elksuiteurl=$ElasticSearchUrl,g" $PROPERTIESFILEPATH
     else
         echo "Missing ELK URL."
         exit 1
     fi
 else
-    sed -i'' -e "s,useelk=no,g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,useelk=,useelk=no,g" $PROPERTIESFILEPATH
 fi
 
 if [[ $DeleteReports ]]; then
-    sed -i'' -e "s,deleteoldreports='$DeleteReports',g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,deleteoldreports=,deleteoldreports=$DeleteReports,g" $PROPERTIESFILEPATH
     if [[ $NumberOfDays ]]; then
-        sed -i'' -e "s,numberofdays='$NumberOfDays',g" $PROPERTIESFILEPATH
+        sed -i'' -e "s,numberofdays=,numberofdays=$NumberOfDays,g" $PROPERTIESFILEPATH
     else
-        sed -i'' -e "s,numberofdays=10,g" $PROPERTIESFILEPATH
+        sed -i'' -e "s,numberofdays=,numberofdays=10,g" $PROPERTIESFILEPATH
     fi
 else
-    sed -i'' -e "s,deleteoldreports=no,g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,deleteoldreports=,deleteoldreports=no,g" $PROPERTIESFILEPATH
 fi
 
 if [[ $OverRideReports ]]; then
-    sed -i'' -e "s,overridereports='$OverRideReports',g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,overridereports=,overridereports=$OverRideReports,g" $PROPERTIESFILEPATH
 else
-    sed -i'' -e "s,overridereports=no,g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,overridereports=,overridereports=no,g" $PROPERTIESFILEPATH
 fi
 
 if [[ $TakeScreenShotForPass ]]; then
-    sed -i'' -e "s,passedscreenshot='$TakeScreenShotForPass',g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,passedscreenshot=,passedscreenshot=$TakeScreenShotForPass,g" $PROPERTIESFILEPATH
 else
-    sed -i'' -e "s,passedscreenshot=no,g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,passedscreenshot=,passedscreenshot=no,g" $PROPERTIESFILEPATH
 fi
 
 if [[ $RetryFailedTestCase ]]; then
-    sed -i'' -e "s,retryfailedtestcases='$RetryFailedTestCase',g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,retryfailedtestcases=,retryfailedtestcases=$RetryFailedTestCase,g" $PROPERTIESFILEPATH
 else
-    sed -i'' -e "s,retryfailedtestcases=no,g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,retryfailedtestcases=,retryfailedtestcases=no,g" $PROPERTIESFILEPATH
 fi
 
 if [[ $SendMailAfterExecution ]]; then
-    sed -i'' -e "s,sendmailafterexecution='$SendMailAfterExecution',g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,sendmailafterexecution=,sendmailafterexecution=$SendMailAfterExecution,g" $PROPERTIESFILEPATH
     if [[ $MailServerName ]]; then
-        sed -i'' -e "s,sendmailusing='$MailServerName',g" $PROPERTIESFILEPATH
+        sed -i'' -e "s,sendmailusing=,sendmailusing=$MailServerName,g" $PROPERTIESFILEPATH
     else
         echo "Missing Mail Server Name."
         exit 1
     fi
     if [[ $SenderEmailID ]]; then
-        sed -i'' -e "s,emailid='$SenderEmailID',g" $PROPERTIESFILEPATH
+        sed -i'' -e "s,emailid=,emailid=$SenderEmailID,g" $PROPERTIESFILEPATH
     else
         echo "Missing Sender's Email ID."
         exit 1
     fi
     if [[ $SenderPassword ]]; then
-        sed -i'' -e "s,emailpassword='$SenderPassword',g" $PROPERTIESFILEPATH
+        sed -i'' -e "s,emailpassword=,emailpassword=$SenderPassword,g" $PROPERTIESFILEPATH
     else
         echo "Missing Sender's Password in Base64Encode."
         exit 1
     fi
     if [[ $ReceiverMailID ]]; then
-        sed -i'' -e "s,receiversemailid='$ReceiverMailID',g" $PROPERTIESFILEPATH
+        sed -i'' -e "s,receiversemailid=,receiversemailid=$ReceiverMailID,g" $PROPERTIESFILEPATH
     else
         echo "Missing Receiver's Email ID."
         exit 1
     fi
 else
-    sed -i'' -e "s,sendmailafterexecution=no,g" $PROPERTIESFILEPATH
+    sed -i'' -e "s,sendmailafterexecution=,sendmailafterexecution=no,g" $PROPERTIESFILEPATH
 fi
