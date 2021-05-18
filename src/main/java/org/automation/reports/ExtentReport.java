bf3d256672e59ab6946b997dd13e93fb9d0f8269
Prashant Singh <pjeet.0733@gmail.com>
2021-05-19 00:34:46 +0400

    Minnor fix

--- src/main/java/org/automation/reports/ExtentReport.java
+++ src/main/java/org/automation/reports/ExtentReport.java
@@ -3,6 +3,7 @@ package org.automation.reports;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import org.automation.constants.GlobalVars;
 import org.automation.enums.ConfigMap;
@@ -59,7 +60,7 @@ public final class ExtentReport {
 			spark.config().setTheme(Theme.DARK);
 			spark.config().setDocumentTitle(GlobalVars.getReportTitle());
 			spark.config().setReportName(GlobalVars.getReportName());
-			spark.config().setEncoding(GlobalVars.getUtf8());
+			spark.config().setEncoding(String.valueOf(StandardCharsets.UTF_8));
 			spark.config().setTimeStampFormat(GlobalVars.getDateTimeFormat2());
 			// spark.config().setOfflineMode(true);
 
